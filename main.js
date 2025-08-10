dee08fdc9c6bb621239872b518bc1770626a9a26
Camilor02 <91165539+Camilor02@users.noreply.github.com>
2025-08-10 01:25:08 +0000

    Avances desde Codespaces: estilos, Bootstrap, JS, secciones

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("¡Proyecto de energía renovable iniciado! ⚡️");
+function mostrarDato() {
+  const datos = [
+    "La Tierra recibe más energía del sol en una hora que toda la humanidad en un año.",
+    "La energía solar no emite gases de efecto invernadero.",
+    "Las placas solares pueden durar más de 25 años.",
+    "Alemania, un país con poco sol, es líder en energía solar en Europa."
+  ];
+
+  const indice = Math.floor(Math.random() * datos.length);
+  document.getElementById("dato").innerText = datos[indice];
+}
+
+// "Probar si toma JS" alert("Hola, el archivo JS se está cargando ✅");
